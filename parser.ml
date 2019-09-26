7cf7b0f8c6e773c1adfb69008a2b169e34b166b2
Lauric Desauw <lauric@Jace.localdomain>
2019-09-26 11:48:23 +0200

    Advanced in the parser, only the cpl have been done

--- parser.ml
+++ parser.ml
@@ -5,27 +5,31 @@ open Token
 (* These are high-level generic functions: they take a parser as argument *)
 (* p? *)
 let opt p = parser
-  | [< x = p >] -> Some x
+    | [< x = p >] -> Some x
   | [<>] -> None
 
-(* p* *)
+  (* p* *)
 let rec many p = parser
-  | [< x = p; l = many p >] -> x :: l
+    | [< x = p; l = many p >] -> x :: l
   | [<>] -> []
 
-(* p+ *)
+  (* p+ *)
 let some p = parser
-  | [< x = p; l = many p >] -> x :: l
+    | [< x = p; l = many p >] -> x :: l
 
-(* Simple example usage: this function count the number of `SEMICOLON' *)
-(* Its takes a Stream of token as argument, and gives an integer *)
+    (* Simple example usage: this function count the number of `SEMICOLON' *)
+    (* Its takes a Stream of token as argument, and gives an integer *)
 let parse_semicolon : token Stream.t -> int = parser
-  | [< 'SEMICOLON >] -> 1 (* Parsers need to match a `SEMICOLON' token *)
+    | [< 'SEMICOLON >] -> 1 (* Parsers need to match a `SEMICOLON' token *)
 
 let count_semicolon = parser
-  | [< l = many parse_semicolon >] -> List.length l (* Parser needs to match `many' `SEMICOLON' and returns the count *)
+    | [< l = many parse_semicolon >] -> List.length l (* Parser needs to match `many' `SEMICOLON' and returns the count *)
 
 
 
-(* let rec parse = parser *)
-(*  Fill here! *);
+let rec parse = parser 
+    | [< L_CHEVRON; STR(x); R_CHEVRON; SEMICOLON >] -> Obj x (* return the cpl in obj version *)
+    | [< QUOT; STR(x); QUOT; SEMICOLON >] -> Txt x (* return the cpl in txt version *)
+    | [< >] -> 
+    ;
+
