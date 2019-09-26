937ff94a9e4db3bb86fdb3d053e0e265cce3a4c3
Lauric Desauw <lauric@Jace.localdomain>
2019-09-26 12:00:06 +0200

    I've made comment in the parser to remind the case to handle.  I don't know how to properly handle them, I think it have to be a recursive way

--- parser.ml
+++ parser.ml
@@ -1,35 +1,12 @@
 open ASD
 open Token
 
-(* This gives some stream parser usage examples. You can discard them once you understood the syntax *)
-(* These are high-level generic functions: they take a parser as argument *)
-(* p? *)
-let opt p = parser
-    | [< x = p >] -> Some x
-  | [<>] -> None
-
-  (* p* *)
-let rec many p = parser
-    | [< x = p; l = many p >] -> x :: l
-  | [<>] -> []
-
-  (* p+ *)
-let some p = parser
-    | [< x = p; l = many p >] -> x :: l
-
-    (* Simple example usage: this function count the number of `SEMICOLON' *)
-    (* Its takes a Stream of token as argument, and gives an integer *)
-let parse_semicolon : token Stream.t -> int = parser
-    | [< 'SEMICOLON >] -> 1 (* Parsers need to match a `SEMICOLON' token *)
-
-let count_semicolon = parser
-    | [< l = many parse_semicolon >] -> List.length l (* Parser needs to match `many' `SEMICOLON' and returns the count *)
-
-
-
 let rec parse = parser 
     | [< L_CHEVRON; STR(x); R_CHEVRON; SEMICOLON >] -> Obj x (* return the cpl in obj version *)
     | [< QUOT; STR(x); QUOT; SEMICOLON >] -> Txt x (* return the cpl in txt version *)
-    | [< >] -> 
+    | [< >] -> (*cpl, _  -> we have to make a cpl list *)
+    (*attr cpl; -> attr of cpl *)
+    (*attr, _ -> we have to make the list of attr *)
+    (*subject attr -> subject of attr *)
     ;
 
