564bce31ed71328603885e98d2a0689a08587e41
adralak <yan.garito@ns-rennes.fr>
2019-10-02 19:30:31 +0200

    Fixed matching + missing spaces

--- ASD.ml
+++ ASD.ml
@@ -5,7 +5,7 @@ type attr = Attr of obj * (cpl list);;
 type ens = Ens of obj * (attr list);;
 type document = Doc of (ens list);;
 
-let read_obj o = "<"^o^">";;
+let read_obj o = "<"^o^"> ";;
 
 let read_cpl c =
     match c with
@@ -14,12 +14,12 @@ let read_cpl c =
 
 let rec read_attr attr =
     match attr with
-    | a,l   -> List.map (function c -> (read_obj a) ^ (read_cpl c)) l
+    | Attr (a,l)   -> List.map (function c -> (read_obj a) ^ (read_cpl c)) l
 ;;
 
 let rec read_ens ens =
     match ens with
-    | a,l -> let l1 = (List.map (function c ->  (read_attr c)) l) in
+    | Ens (a,l) -> let l1 = (List.map (function c ->  (read_attr c)) l) in
              let reduce = function c -> (List.fold_left (fun x y ->  x ^ (read_obj a) ^ y ^ ".\n ") "" c) in
              let l2 = List.map reduce l1 in
              List.fold_left (fun x y -> x ^ y) "" l2
@@ -27,8 +27,8 @@ let rec read_ens ens =
 
 let rec ntriples_of_ast doc =
     match doc with
-    | []   -> ""
-    | p::q -> read_ens(p)^ntriples_of_ast(q)
+    | Doc []   -> ""
+    | Doc (p::q) -> read_ens(p)^ntriples_of_ast(Doc q)
 ;;
 
 (* Function to generate the document out of the AST*)
@@ -42,12 +42,12 @@ let obj2 = Obj("banane");;
 let obj3 = Txt("PDS");;
 let obj4 = Obj(suj2);;
 
-let attr1 = verb1,obj4::obj3::[];;
-let attr2 = verb1,obj1::[];;
-let attr3 = verb2,obj2::[];;
+let attr1 = Attr (verb1,obj4::obj3::[]);;
+let attr2 = Attr (verb1,obj1::[]);;
+let attr3 = Attr (verb2,obj2::[]);;
 
-let ens1 = suj1,attr1::[];;
-let ens2 = suj2,attr2::attr3::[];;
+let ens1 = Ens (suj1,attr1::[]);;
+let ens2 = Ens (suj2,attr2::attr3::[]);;
 
-let doc = ens1::ens2::[];;
-ntriples_of_ast doc;;
+let doc = Doc (ens1::ens2::[]);;
+print_string (ntriples_of_ast doc);;
