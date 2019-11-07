429f47b66c1a18cbac7c0652f486e453bca0dfcc
adralak <yan.garito@ns-rennes.fr>
2019-11-07 08:21:15 +0100

    ASD for declaration

--- Tp2/ocaml/ASD.ml
+++ Tp2/ocaml/ASD.ml
@@ -2,7 +2,8 @@
 
 type ident = string
 
-
+type typ =
+  | Type_Int
 
 type expression =
   | AddExpression of expression * expression
@@ -13,14 +14,11 @@ type expression =
   | VarExpression of string
 
 type instruction =
-    | AffectInstruction of string * expression
-
-
+  | AffectInstruction of string * expression
+  | DeclInstruction of typ * string
+                         
 type codeObj = Expr of expression | Instr of instruction | Bloc of (codeObj list)
 
 type bloc = codeObj list 
-
-type typ =
-  | Type_Int
 
 type program = codeObj list
